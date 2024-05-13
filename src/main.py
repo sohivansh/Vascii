04253ec1f1be7cad6f77e635c1d2e1f3a8ce7e17
Vansh Sohi <99751969+sohivansh@users.noreply.github.com>
2024-05-13 18:46:10 -0400

    Converted video to grayscale

--- src/main.py
+++ src/main.py
@@ -4,23 +4,15 @@ import numpy as np
 def main():
 
     vid = cv.VideoCapture('assets/test.mp4')
+    
 
     while True:
         ret, frame = vid.read()
-        new_frame = cv.resize(frame,(1200,1200))
-        new_frame = cv.resize(new_frame,(0,0),fx=0.5,fy=0.5)
-        for i in new_frame.height:
-            for j in vid.get(4):
-                average = (new_frame[i][j][0]+new_frame[i][j][1]+new_frame[i][j][2])//3
+        gray_frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        height, width = gray_frame.shape
+        gray_frame = cv.resize(gray_frame,(width//4,height//4))
+        key = cv.waitKey(1)
 
-        
-
-            
-        
-        cv.imshow('Video',new_frame)
-
-
-        key = cv.waitKey(1) 
         if key == ord('q'):
             break
 
